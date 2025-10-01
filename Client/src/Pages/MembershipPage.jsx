a84642a2f45dba98347267b69d53ca57d8ee1173
Long2ej <long2ej@dukes.jmu.edu>
2025-09-30 20:33:42 -0400

    Update

--- Client/src/Pages/MembershipPage.jsx
+++ Client/src/Pages/MembershipPage.jsx
@@ -10,7 +10,7 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">Day Pass Membership</h1>
-                    <p className="price">$20</p>
+                    <p className="price">$30</p>
                     <p className="descriptionofpurchase">Each Day</p>
                     <p className="descriptionofmem">Day-by-day access without a long term commitment</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
@@ -20,7 +20,7 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">One Week Membership</h1>
-                    <p className="price">$35</p>
+                    <p className="price">$50</p>
                     <p className="descriptionofpurchase">Each Week</p>
                     <p className="descriptionofmem">Week-by-week payment without a longer term commitment</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
@@ -30,7 +30,7 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">One Month Membership</h1>
-                    <p className="price">$80</p>
+                    <p className="price">$95</p>
                     <p className="descriptionofpurchase">Each Month</p>
                     <p className="descriptionofmem">Stick around for a bit with a monthly commitment</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
@@ -40,7 +40,7 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">Two Month Membership</h1>
-                    <p className="price">$140</p>
+                    <p className="price">$180</p>
                     <p className="descriptionofpurchase">Every Two Months</p>
                     <p className="descriptionofmem">Two months worth of membership</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
@@ -50,7 +50,7 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">Three Month Membership</h1>
-                    <p className="price">$195</p>
+                    <p className="price">$225</p>
                     <p className="descriptionofpurchase">Every Three Months</p>
                     <p className="descriptionofmem">A full season worth of membership</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
@@ -60,9 +60,9 @@ const MembershipPage = () => {
 
                 <div className="memboxes">
                     <h1 className="head">Annual Membership</h1>
-                    <p className="price">$60</p>
+                    <p className="price">$70</p>
                     <p className="descriptionofpurchase">Each Month</p>
-                    <p className="descriptionofmemannual">Full year access our most popular plan<br />*$79 initiation fee at contract signing + first and last monthly fees paid up front*</p>
+                    <p className="descriptionofmemannual">Full year access our most popular plan<br />*$79 initiation fee & one time yearly maintenance fee of $70*</p>
                     <a href="https://clients.mindbodyonline.com/ASP/su1.asp?studioid=156543&tg=&vt=&lvl=&stype=&view=&trn=0&page=&catid=&prodid=&date=5%2f31%2f2023&classid=0&prodGroupId=&sSU=&optForwardingLink=&qParam=&justloggedin=&nLgIn=&pMode=0&loc=1" target="_blank">
                         <button className="action_btn4">Join Now</button>
                     </a>
